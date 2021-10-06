515e504ad69732dfb8b7efe0560addb1817c9941
madhuiitb <54939657+madhuiitb@users.noreply.github.com>
2021-10-06 19:52:12 +0530

    Create TwoSum.js
    1. Two Sum | Leet code | Java Script

---
+++ TwoSum.js
@@ -0,0 +1,22 @@
+#1. Two Sum
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    var sum;
+    
+    var len = nums.length;
+    for(var i =0; i<len;i++){
+        for(var j=i+1;j<len;j++){
+            if(nums[i]+nums[j]==target){
+                return [i,j];
+            }
+        }
+    }
+    
+};
