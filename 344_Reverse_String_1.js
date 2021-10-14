d701ed6a338590f42bbfe57cbe500106d0237002
madhuiitb <54939657+madhuiitb@users.noreply.github.com>
2021-10-14 23:10:17 +0530

    Update 344_Reverse_String_1.js

--- 344_Reverse_String_1.js
+++ 344_Reverse_String_1.js
@@ -5,7 +5,10 @@
  
  * Runtime: 1488 ms, faster than 5.04% of JavaScript online submissions for Reverse String.
  * Memory Usage: 47.5 MB, less than 11.26% of JavaScript online submissions for Reverse String.
- 
+ * Using Javascript functionalities i.e Splice is good.
+ * Splice functionality is spicle(start_position, no_of_items_to_delete, item1, item2, item3,....itemN)
+ * splice(start_position)
+ * splice(start_position, no_of_items_to_delete)
 
  */
 var reverseString = function(s) {
